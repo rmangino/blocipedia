a89dbb5dfd5c0159ce2143f7aac6a57e63898ba0
Reed Mangino <reed@themanginos.com>
2015-08-03 08:04:33 -0400

    Fixed a bug in the way we were finding collaborators.

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -33,7 +33,7 @@ class CollaboratorsController < ApplicationController
 private
 
   def find_collaborator
-    @collaborator = @wiki.collaborators.find(current_user.id)
+    @collaborator = @wiki.collaborators.find_by(user_id: params[:user_id])
   end
 
   def find_wiki
